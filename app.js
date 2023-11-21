32eff6fd542b2086157d173259bb639f2ec3a6d8
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2023-11-21 18:45:30 +0700

    cors

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const morgan = require('morgan');
 const routers = require('./routers');
 const ApiError = require('./utils/apiError');
 const errorHandler = require('./controllers/errorController');
+const cors = require('cors');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 app.use(routers);
 app.all('*', (req, res, next) => {
   next(new ApiError(`Routes does not exist`, 404));
